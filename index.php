13e16ebd19ae0aa186c6b2ca633cb4551c481992
sayat23 <sayat23@mail.ru>
2020-05-11 11:55:51 +0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,534 @@
+<!DOCTYPE html>
+<!--[if IE 8 ]>
+<html class="no-js oldie ie8" lang="ru"> <![endif]-->
+<!--[if IE 9 ]>
+<html class="no-js oldie ie9" lang="ru"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+
+    <!--- basic page needs
+    ================================================== -->
+    <meta charset="utf-8">
+    <title>SPA салон BRUTT</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- mobile specific metas
+    ================================================== -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- CSS
+  ================================================== -->
+    <link rel="stylesheet" href="css/base.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/vendor.css">
+
+
+    <!-- script
+    ================================================== -->
+    <script src="js/modernizr.js"></script>
+
+    <!-- favicons
+     ================================================== -->
+    <link rel="icon" type="image/png" href="favicon.png">
+
+</head>
+
+<body id="top">
+
+<!-- header
+================================================== -->
+<header>
+
+    <div class="row">
+
+        <div class="logo">
+            <a href="/">Боди масссаж</a>
+        </div>
+        <div class="brutt">SPA салон "Brutt"</div>
+
+        <nav id="main-nav-wrap">
+            <ul class="main-navigation">
+                <li class="current"><a class="smoothscroll" href="#intro" title="">Главная</a></li>
+                <li><a class="smoothscroll" href="#process" title="">О НАС</a></li>
+                <li><a class="smoothscroll" href="#features" title="">ПРЕИМУЩЕСТВА</a></li>
+                <li><a class="smoothscroll" href="#cta" title="">СОЦСЕТИ</a></li>
+                <li><a class="smoothscroll" href="#kon" title="">КОНТАКТЫ</a></li>
+                <li class="highlight with-sep"><a href="tel:++77075881676" title="">+7 707 588 16 76</a></li>
+            </ul>
+        </nav>
+
+        <a class="menu-toggle" href="#"><span>Menu</span></a>
+
+    </div>
+
+</header> <!-- /header -->
+
+<!-- intro section
+================================================== -->
+<section id="intro">
+
+    <div class="shadow-overlay"></div>
+
+    <div class="intro-content">
+        <div class="row">
+
+            <div class="col-twelve">
+
+                <h5>Мужской спа-салон</h5>
+                <h1>Незабываемый боди массаж</h1>
+
+                <a class="button stroke"
+                   href="https://wa.me/77075881676?text=Здравствуйте,%20я%20насчет%20боди%20массажа"
+                   title="">Написать</a>
+
+            </div>
+
+        </div>
+    </div>
+
+</section> <!-- /intro -->
+
+<!-- Process Section
+================================================== -->
+<section id="process">
+
+    <div class="row section-intro">
+        <div class="col-twelve with-bottom-line">
+
+            <!--   			<h5>ФОТО БЛОК</h5>-->
+            <h1>Девушки BRUTT-spa</h1>
+
+            <p class="lead">Незабываемый, возбуждающий массаж обнаженным, стройным, молодым телом красивой девушки
+                подарит Вам эротическое удовольствие и сексуальное расслабление! У всех девушек имеется сертификат
+                массажиста!</p>
+            <!--   			<p class="lead">Здесь может блок фотографии девушек в работе и тд</p>-->
+
+        </div>
+    </div>
+
+    </div> <!-- /process-content -->
+
+</section> <!-- /process-->
+
+
+<!-- features Section
+================================================== -->
+<section id="features">
+
+    <div class="row section-intro">
+        <div class="col-twelve with-bottom-line">
+
+            <h5>Наши преимущества</h5>
+            <h1>Мы предоставляем самый качественный релакс</h1>
+
+            <p class="lead">Обратие внимание, мы не предоставляем интимные услуги</p>
+
+        </div>
+    </div>
+
+    <div class="row features-content">
+        <div class="features-list block-1-3 block-s-1-2 block-tab-full group">
+            <div class="bgrid feature">
+                <span class="icon"><i class="icon-window"></i></span>
+                <div class="service-content">
+                    <h3 class="h05">Большой выбор программ</h3>
+                    <p>В нашем салоне представлено более 15 программ на любой вкус.</p>
+                </div>
+            </div> <!-- /bgrid -->
+            <div class="bgrid feature">
+                <span class="icon"><i class="icon-eye"></i></span>
+                <div class="service-content">
+                    <h3 class="h05">Уютная атмосфера</h3>
+                    <p>Комнаты с джакузи и комнаты оснащенные душевыми кабинками.</p>
+                </div>
+            </div> <!-- /bgrid -->
+            <div class="bgrid feature">
+                <span class="icon"><i class="icon-paint-brush"></i></span>
+                <div class="service-content">
+                    <h3 class="h05">Великолепные девушки</h3>
+                    <p>Профессиональные мастера релакса с волшебными руками и восхитительными формами</p>
+                </div>
+            </div> <!-- /bgrid -->
+            <div class="bgrid feature">
+                <span class="icon"><i class="icon-file"></i></span>
+                <div class="service-content">
+                    <h3 class="h05">Большой ассортимент напитков и кальяна</h3>
+                    <p>Махровые полотенца, одноразовое чистое белье, гели для душа и тапочки. Мы всё продумали.</p>
+                </div>
+            </div> <!-- /bgrid -->
+            <div class="bgrid feature">
+                <span class="icon"><i class="icon-layers"></i></span>
+                <div class="service-content">
+                    <h3 class="h05">Полная конфиденциальность</h3>
+                    <p>Фото-Видео съемка в салоне запрещена. Гости не пересекаются внутри салона.</p>
+                </div>
+            </div> <!-- /bgrid -->
+            <div class="bgrid feature">
+                <span class="icon"><i class="icon-gift"></i></span>
+                <div class="service-content">
+                    <h3 class="h05">24/7</h3>
+                    <p>Мы работаем 24 часа в сутки без праздников и выходных. Всё для вас мужчины!</p>
+                </div>
+            </div> <!-- /bgrid -->
+        </div> <!-- features-list -->
+    </div> <!-- features-content -->
+</section> <!-- /features -->
+
+<!-- pricing
+================================================== -->
+
+<!--   <section id="pricing">-->
+
+<!--   	<div class="row section-intro">-->
+<!--   		<div class="col-twelve with-bottom-line">-->
+
+<!--   			<h5>Наши программы и цены</h5>-->
+<!--   			<h1>Выберите лучшее предложение для вас</h1>-->
+
+<!--   			<p class="lead">Здесь должен быть такст типа : "Здесь вам представлены цены на наши услуши боди массажа и тд и тп"</p>-->
+
+<!--   		</div>-->
+<!--   	</div>-->
+
+<!--   	<div class="row pricing-content">-->
+
+<!--         <div class="pricing-tables block-1-4 group">-->
+
+<!--            <div class="bgrid">-->
+
+<!--            	<div class="price-block">-->
+
+<!--            		<div class="top-part">-->
+
+<!--	            		<h3 class="plan-title">Базовый</h3>-->
+<!--		               <p class="plan-price"><sup></sup>12 000</p>-->
+<!--						<p class="price-month">40 минут</p>-->
+
+<!--	            	</div>-->
+
+<!--	               <div class="bottom-part">-->
+
+<!--		               <a class="button large" href="">Записаться</a>-->
+
+<!--	            	</div>-->
+
+<!--            	</div>-->
+
+<!--			   </div> &lt;!&ndash; /price-block &ndash;&gt;-->
+
+<!--            <div class="bgrid">-->
+
+<!--            	<div class="price-block">-->
+
+<!--					<div class="top-part">-->
+
+<!--						<h3 class="plan-title">Классика</h3>-->
+<!--						<p class="plan-price"><sup></sup>25 000</p>-->
+<!--						<p class="price-month">90 минут</p>-->
+<!--					</div>-->
+
+<!--					<div class="bottom-part">-->
+
+<!--		               <a class="button large" href="">Записаться</a>-->
+
+<!--	            	</div>-->
+
+<!--            	</div>-->
+
+<!--			  </div> &lt;!&ndash; /price-block &ndash;&gt;-->
+
+<!--           <div class="bgrid">-->
+
+<!--               <div class="price-block">-->
+
+<!--            		<div class="top-part">-->
+
+<!--	            		<h3 class="plan-title">Премиум</h3>-->
+<!--		               <p class="plan-price"><sup></sup>25 000</p>-->
+<!--		               <p class="price-month">90 минут</p>-->
+
+<!--	            	</div>-->
+
+<!--						<div class="bottom-part">-->
+
+<!--		               <a class="button large" href="">Записаться</a>-->
+
+<!--	            	</div>-->
+
+<!--            	</div>-->
+
+<!--			   </div> &lt;!&ndash; /price-block &ndash;&gt;-->
+
+<!--			   <div class="bgrid">-->
+
+<!--               <div class="price-block">-->
+
+<!--            		<div class="top-part">-->
+
+<!--	            		<h3 class="plan-title">Максимум</h3>-->
+<!--		               <p class="plan-price"><sup></sup>30 000</p>-->
+<!--		               <p class="price-month">90 минут</p>-->
+
+<!--	            	</div>-->
+
+<!--						<div class="bottom-part">-->
+
+
+<!--		               <a class="button large" href="">Записаться</a>-->
+
+<!--	            	</div>-->
+
+<!--            	</div>-->
+
+<!--			   </div> &lt;!&ndash; /price-block &ndash;&gt;-->
+
+<!--         </div> &lt;!&ndash; /pricing-tables &ndash;&gt;-->
+
+<!--      </div> &lt;!&ndash; /pricing-content &ndash;&gt;-->
+
+<!--   </section> &lt;!&ndash; /pricing &ndash;&gt;-->
+
+
+<!--   	<div class="row section-intro">-->
+<!--   		<div class="col-twelve with-bottom-line">-->
+<!-- Testimonials Section
+================================================== -->
+<!--   <section id="testimonials">-->
+
+<!--   	<div class="row">-->
+<!--   		<div class="col-twelve">-->
+<!--   			<h2 class="h01">Отзывы наших клиентов</h2>-->
+<!--   		</div>-->
+<!--   	</div>-->
+
+<!--      <div class="row flex-container">-->
+
+<!--         <div id="testimonial-slider" class="flexslider">-->
+
+<!--            <ul class="slides">-->
+
+<!--               <li>-->
+<!--               	<div class="testimonial-author">-->
+<!--                    	<img src="images/avatars/avatar-1.jpg" alt="Author image">-->
+<!--                    	<div class="author-info">-->
+<!--                    		Иванов Иван-->
+<!--                    	</div>-->
+<!--                  </div>-->
+
+<!--                  <p>-->
+<!--                  Некоторый текст с отзывом о салоне. Некоторый текст с отзывом о салоне.Некоторый текст с отзывом о салоне.Некоторый текст с отзывом о салоне.Некоторый текст с отзывом о салоне.-->
+<!--				  </p>-->
+<!--             	</li> &lt;!&ndash; /slide &ndash;&gt;-->
+
+<!--               <li> -->
+
+<!--               	<div class="testimonial-author">-->
+<!--                    	<img src="images/avatars/avatar-2.jpg" alt="Author image">-->
+<!--                    	<div class="author-info">-->
+<!--                    		Олжас Багат-->
+<!--                    	</div>-->
+<!--                  </div> -->
+
+<!--                  <p>-->
+<!--					  Некоторый текст с отзывом о салоне.Некоторый текст с отзывом о салоне.Некоторый текст с отзывом о салоне.Некоторый текст с отзывом о салоне.Некоторый текст с отзывом о салоне.Некоторый текст с отзывом о салоне.Некоторый текст с отзывом о салоне.Некоторый текст с отзывом о салоне.-->
+<!--				  </p>-->
+<!--                                         -->
+<!--               </li> &lt;!&ndash; /slide &ndash;&gt;-->
+
+<!--            </ul> &lt;!&ndash; /slides &ndash;&gt;-->
+
+<!--         </div> &lt;!&ndash; /testimonial-slider &ndash;&gt;         -->
+<!--        -->
+<!--      </div> &lt;!&ndash; /flex-container &ndash;&gt;-->
+
+<!--   </section> &lt;!&ndash; /testimonials &ndash;&gt;-->
+
+
+<!--	<section id="workstation">-->
+
+<!--		<div class="row section-intro">-->
+<!--			<div class="col-twelve with-bottom-line">-->
+<!--				<h1>Наши мастера</h1>-->
+<!--			</div>-->
+<!--		</div>-->
+
+
+<!--		<div class="stats-line">-->
+
+<!--			<div class="square">-->
+<!--				<span class="stats-name">ЭЛЯ, 19 лет</span>-->
+<!--				<img src="images/1.jpg" class="stats-img">-->
+<!--				<div class="stats-text">Некий текст некий текст некий текст некий текст некий текст</div>-->
+<!--			</div>-->
+
+<!--			<div class="square">-->
+<!--				<span class="stats-name">ЭЛЯ, 19 лет</span>-->
+<!--				<img src="images/1.jpg" class="stats-img">-->
+<!--				<div class="stats-text">Некий текст некий текст некий текст некий текст некий текст</div>-->
+<!--			</div>-->
+
+<!--			<div class="square">-->
+<!--				<span class="stats-name">ЭЛЯ, 19 лет</span>-->
+<!--				<img src="images/1.jpg" class="stats-img">-->
+<!--				<div class="stats-text">Некий текст некий текст некий текст некий текст некий текст</div>-->
+<!--			</div>-->
+
+<!--			<div class="square">-->
+<!--				<span class="stats-name">ЭЛЯ, 19 лет</span>-->
+<!--					<img src="images/1.jpg" class="stats-img">-->
+<!--				<div class="stats-text">Некий текст некий текст некий текст некий текст некий текст</div>-->
+<!--			</div>-->
+
+<!--			<div class="square">-->
+<!--				<span class="stats-name">ЭЛЯ, 19 лет</span>-->
+<!--				<img src="images/1.jpg" class="stats-img">-->
+<!--				<div class="stats-text">Некий текст некий текст некий текст некий текст некий текст</div>-->
+<!--			</div>-->
+
+
+<!--		</div>-->
+
+<!--	</section>-->
+
+
+<!-- cta
+================================================== -->
+<section id="cta">
+
+    <div class="row cta-content">
+
+        <div class="col-twelve">
+
+            <h1 class="h01">Позвони или напиши сейчас.</h1>
+
+            <ul class="stores">
+                <li class="app-store">
+                    <a href="https://wa.me/77075881676?text=Здравствуйте,%20я%20насчет%20боди%20массажа"
+                       class="button round large" title="">
+                        <i class="icon ion-social-whatsapp"></i>WatsApp
+                    </a>
+                </li>
+                <li class="play-store">
+                    <a href="https://www.instagram.com/brutt.spa01/?igshid=1c8r8huhnlzf" class="button round large"
+                       title="">
+                        <i class="icon ion-social-instagram"></i>Instagram</a>
+                </li>
+
+            </ul>
+
+        </div>
+
+    </div> <!-- /cta-content -->
+
+</section> <!-- /cta -->
+
+
+<!-- footer
+================================================== -->
+<footer>
+
+    <div class="footer-main" id="kon">
+
+        <div class="row">
+
+            <div class="col-four tab-full mob-full footer-info">
+
+                <div class="footer-logo"></div>
+
+                <p>
+                    010 000<br>
+                    г.Нур-Султан, Ақберен переулок, 7 <br>
+                    +7 707 588 16 76
+                </p>
+
+            </div> <!-- /footer-info -->
+
+            <div class="col-two tab-1-3 mob-1-2 site-links">
+
+                <h4>Ссылки</h4>
+
+                <ul>
+                    <li class="current"><a class="smoothscroll" href="#intro" title="">Главная</a></li>
+                    <li><a class="smoothscroll" href="#process" title="">О нас</a></li>
+                    <li><a class="smoothscroll" href="#features" title="">Преимущества</a></li>
+                    <!--					<li><a class="smoothscroll"  href="#faq" title="">Мастера</a></li>-->
+                    <!--					<li><a class="smoothscroll"  href="#pricing" title="">Цены</a></li>-->
+                </ul>
+
+            </div> <!-- /site-links -->
+
+            <div class="col-two tab-1-3 mob-1-2 social-links">
+                <h4>Мы в соцсетях</h4>
+                <ul>
+                    <li>
+                        <a href="https://wa.me/77075881676?text=Здравствуйте,%20я%20насчет%20боди%20массажа">WhatsApp</a>
+                    </li>
+                    <li><a href="https://www.instagram.com/brutt.spa01/?igshid=1c8r8huhnlzf">Instagram</a></li>
+                </ul>
+            </div> <!-- /social -->
+
+            <div class="col-four tab-1-3 mob-full footer-subscribe">
+
+                <!--	      		<h4>Подписка</h4>-->
+
+                <!--	      		<p>Держите себя в курсе всех акции и скидок. Подпишитесь.</p>-->
+
+                <!--	      		<div class="subscribe-form">-->
+
+                <!--	      			<form id="mc-form" class="group" novalidate="true">-->
+
+                <!--							<input type="email" value="" name="dEmail" class="email" id="mc-email" placeholder="введите адрес эл.почты и нажмите Enter" required="">-->
+
+                <!--			   			<input type="submit" name="subscribe" >-->
+
+                <!--		   				<label for="mc-email" class="subscribe-message"></label>-->
+
+                <!--						</form>-->
+
+                <!--	      		</div>-->
+
+            </div> <!-- /subscribe -->
+
+        </div> <!-- /row -->
+
+    </div> <!-- /footer-main -->
+
+
+    <div class="footer-bottom">
+
+        <div class="row">
+
+            <div class="col-twelve">
+                <div class="copyright">
+                    <span>© Copyright SPA салон "BRUTT"</span>
+
+                </div>
+
+                <div id="go-top" style="display: block;">
+                    <a class="smoothscroll" title="Back to Top" href="#top"><i
+                                class="icon ion-android-arrow-up"></i></a>
+                </div>
+            </div>
+
+        </div> <!-- /footer-bottom -->
+
+    </div>
+
+</footer>
+
+<div id="preloader">
+    <div id="loader"></div>
+</div>
+
+<!-- Java Script
+================================================== -->
+<script src="js/jquery-1.11.3.min.js"></script>
+<script src="js/jquery-migrate-1.2.1.min.js"></script>
+<script src="js/plugins.js"></script>
+<script src="js/main.js"></script>
+
+</body>
+
+</html>
